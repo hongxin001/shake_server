c473ae71671704335aab363db7bda206ed3b562e
魏鸿鑫 <821881341@qq.com>
2015-11-16 14:25:29 +0800

    modified:   .gitignore 	modified:   server-modules/config.js

--- server-modules/config.js
+++ server-modules/config.js
@@ -22,8 +22,8 @@ let config = {
     'http://localhost:9000',
     'http://localhost:3000',
     // 以下两个是在 LeanCloud 中配置的 host，xxx 替换为自己的域名
-    'http://dev.xxx.avosapps.com',
-    'http://xxx.avosapps.com'
+    'http://dev.shake.avosapps.com',
+    'http://shake.avosapps.com'
   ]
 };
 
@@ -32,14 +32,14 @@ switch (process.env.LC_APP_ENV) {
 
   // 当前环境为线上测试环境
   case 'stage':
-    config.host = 'http://dev.xxx.avosapps.com';
-    config.webHost = 'http://dev.xxx.avosapps.com';
+    config.host = 'http://dev.shake.avosapps.com';
+    config.webHost = 'http://dev.shake.avosapps.com';
   break;
 
   // 当前环境为线上正式运行的环境
   case 'production':
-    config.host = 'http://xxx.avosapps.com';
-    config.webHost = 'http://xxx.avosapps.com';
+    config.host = 'http://shake.avosapps.com';
+    config.webHost = 'http://shake.avosapps.com';
   break;
 }
 
